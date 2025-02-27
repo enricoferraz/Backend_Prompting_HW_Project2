340bb15370a9ca181ece23f9bd75291f2b89cfc6
enricoferraz <enricofferraz@gmail.com>
2025-02-27 20:44:47 -0300

    fix: adjust type hints

--- functions/chatgpt_api.py
+++ functions/chatgpt_api.py
@@ -3,13 +3,13 @@ from entities.prompts import prompt_1, prompt_2
 from entities import NegativeTreatment
 from dotenv import load_dotenv
 import os
-
+import json
 
 load_dotenv('.env')
 
 client = OpenAI(api_key=os.environ.get("OPENAI_KEY"))
 
-def get_response_from_gpt(decision: str):
+def get_response_from_gpt(decision: str) -> str | dict:
     #Using 4o-mini as its cheaper and better than 3.5-turbo
     #Also accepts strutured outputs
     response = client.beta.chat.completions.parse(
@@ -27,5 +27,4 @@ def get_response_from_gpt(decision: str):
         response_format=NegativeTreatment
     )
 
-    #print(response.choices[0].message.content);
-    return response.choices[0].message.content
+    return json.dumps(json.loads(response.choices[0].message.content), indent=2)
